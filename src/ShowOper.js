b04fbc07e53b737b874f28bb358d25ffc5178e78
JeonH503 <wjs42222naver.com>
2020-02-10 16:48:14 +0900

    선택된 태그 표시되도록 완료

--- src/ShowOper.js
+++ src/ShowOper.js
@@ -3,32 +3,34 @@ import './App.css';
 import data from './char.json';
 
 function showResult (selectedTags) {
+  let jsonData = JSON.parse(JSON.stringify(data));//변수가 포인터로 되는것을 방지
   let temp = [];
-  data.forEach(element => {
+  jsonData.forEach(element => {
     for(let i=0; i<selectedTags.tags.length; i++){
       let currentTagIndex = element.tag.indexOf(selectedTags.tags[i])
       if(currentTagIndex !== -1){
         if(temp[temp.length-1]  && temp[temp.length-1].name === element.name){
+          temp[temp.length-1].tag[currentTagIndex] = '<span style="color:red">'+selectedTags.tags[i]+'</span>';
           continue;
         }
         temp.push(element);
-        temp[temp.length-1].selectedTags = selectedTags.tags;
+        temp[temp.length-1].tag[currentTagIndex] = '<span style="color:red">'+selectedTags.tags[i]+'</span>';
       }
     }
   });
   return temp;
 }
 
+let result = [];
 function ShowOper(props) {
-  let result = showResult(props);
-  console.log(result);
+  result = showResult(props);
   return (
     <div>
       {result.map( data => {
         return (
           <div>
             <p>{data.name}</p>
-            <p>{data.tag}</p>
+            <p dangerouslySetInnerHTML={ {__html: data.tag}}/>
           </div>
           )
       })}
