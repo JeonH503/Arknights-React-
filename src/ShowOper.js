6ffdea8b62bc80b498ed9b8aa692345bf2c40290
JeonH503 <wjs42222naver.com>
2020-02-10 22:24:59 +0900

    주석 추가, 캐릭터 중복 표시 수정

--- src/ShowOper.js
+++ src/ShowOper.js
@@ -5,32 +5,43 @@ import data from './char.json';
 function showResult (selectedTags) {
   let jsonData = JSON.parse(JSON.stringify(data));//변수가 포인터로 되는것을 방지
   let temp = [];
-  jsonData.forEach(element => {
-    for(let i=0; i<selectedTags.tags.length; i++){
+
+  if(selectedTags.tags.indexOf("고급특별채용") === -1){ //고급 특별채용 태그가 선택 되지 않으면 6성캐릭 표시X
+    jsonData = jsonData.splice(7);
+  }
+
+  jsonData.forEach(element => { //70개의 캐릭터들의 태그중 선태된 태그와 같은 태그가 있다면 push
+    for(let i=0; i<selectedTags.tags.length; i++){//선택된 태그 수 만큼 반복
       let currentTagIndex = element.tag.indexOf(selectedTags.tags[i])
-      if(currentTagIndex !== -1){
-        if(temp[temp.length-1]  && temp[temp.length-1].name === element.name){
-          temp[temp.length-1].tag[currentTagIndex] = '<span style="color:red">'+selectedTags.tags[i]+'</span>';
+      if(currentTagIndex !== -1){//element의 태그와 선택된 태그에 공통된 태그가 있다면 실행
+        console.log(selectedTags.tags[i]);
+        if(temp[temp.length-1]  && temp[temp.length-1].name === element.name){//temp의 중복값 push 방지
+          temp[temp.length-1].selectedTagsCount++; //일치한 태그 수 만큼 +된다
+          temp[temp.length-1].tag[currentTagIndex] = '<span style="color:red">'+selectedTags.tags[i]+'</span>';//선택된 태그와 캐릭터 태그와 같으면 글자색 붉은색으로 설정
           continue;
         }
         temp.push(element);
-        temp[temp.length-1].tag[currentTagIndex] = '<span style="color:red">'+selectedTags.tags[i]+'</span>';
+        temp[temp.length-1].selectedTagsCount++; //일치한 태그 수 만큼 +된다
+        temp[temp.length-1].tag[currentTagIndex] = '<span style="color:red">'+selectedTags.tags[i]+'</span>';//선택된 태그와 캐릭터 태그와 같으면 글자색 붉은색으로 설정
       }
     }
   });
   return temp;
 }
 
-let result = [];
 function ShowOper(props) {
-  result = showResult(props);
+  let result = showResult(props);
+  result.sort((a,b) => {
+    return a.selectedTagsCount > b.selectedTagsCount ? -1 : 1;
+  }); //result배열의 선택된 태그 값에 따라 내림차순으로 정렬
   return (
     <div>
       {result.map( data => {
         return (
           <div>
-            <p>{data.name}</p>
+            <p>{data.rare}☆ {data.name}</p>
             <p dangerouslySetInnerHTML={ {__html: data.tag}}/>
+            <p>{data.selectedTagsCount}</p>
           </div>
           )
       })}
