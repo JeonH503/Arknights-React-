0d699e31efe60cc0e9084fe57a34587987c34e07
JeonH503 <wjs42222naver.com>
2020-02-16 01:03:21 +0900

    Css Update

--- src/App.js
+++ src/App.js
@@ -35,36 +35,48 @@ function App() {
         </div>
 
         <div className="class">
-          <p className="가드" onClick={activeEvent}>가드</p>
-          <p className="디펜더" onClick={activeEvent}>디펜더</p>
-          <p className="메딕" onClick={activeEvent}>메딕</p>
-          <p className="뱅가드" onClick={activeEvent}>뱅가드</p>
+          <div>
+            <p className="가드" onClick={activeEvent}>가드</p>
+            <p className="디펜더" onClick={activeEvent}>디펜더</p>
+            <p className="메딕" onClick={activeEvent}>메딕</p>
+            <p className="뱅가드" onClick={activeEvent}>뱅가드</p>
+          </div>
+          <div>
           <p className="서포터" onClick={activeEvent}>서포터</p>
           <p className="스나이퍼" onClick={activeEvent}>스나이퍼</p>
           <p className="스페셜리스트" onClick={activeEvent}>스페셜리스트</p>
           <p className="캐스터" onClick={activeEvent}>캐스터</p>
+          </div>
         </div>
 
         <div className="skill">
+          <div>
           <p className="감속" onClick={activeEvent}>감속</p>
           <p className="강제이동" onClick={activeEvent}>강제이동</p>
           <p className="누커" onClick={activeEvent}>누커</p>
           <p className="디버프" onClick={activeEvent}>디버프</p>
-          <p className="딜러" onClick={activeEvent}>딜러</p>
-          <p className="방어형" onClick={activeEvent}>방어형</p>
-          <p className="범위공격" onClick={activeEvent}>범위공격</p>
-          <p className="생존형" onClick={activeEvent}>생존형</p>
-          <p className="서포트" onClick={activeEvent}>서포트</p>
-          <p className="소환" onClick={activeEvent}>소환</p>
-          <p className="제어형" onClick={activeEvent}>제어형</p>
-          <p className="코스트+" onClick={activeEvent}>코스트+</p>
-          <p className="힐링" onClick={activeEvent}>힐링</p>
+          </div>
+          <div>
+            <p className="딜러" onClick={activeEvent}>딜러</p>
+            <p className="방어형" onClick={activeEvent}>방어형</p>
+            <p className="범위공격" onClick={activeEvent}>범위공격</p>
+            <p className="생존형" onClick={activeEvent}>생존형</p>
+            <p className="서포트" onClick={activeEvent}>서포트</p>
+          </div>
+          <div>
+            <p className="소환" onClick={activeEvent}>소환</p>
+            <p className="제어형" onClick={activeEvent}>제어형</p>
+            <p className="코스트+" onClick={activeEvent}>코스트+</p>
+            <p className="힐링" onClick={activeEvent}>힐링</p>
+          </div>
         </div>
       </div>
-      {selectedTags.map( tag => (
-        <span key={tag}>{tag}</span>
-      ))}
-      <ShowOper tags = {selectedTags}/>
+      <div class = "result">
+        {selectedTags.map( tag => (
+          <span key={tag}>{tag} </span>
+        ))}
+        <ShowOper tags = {selectedTags}/>
+      </div>
     </div>
   );
 }
